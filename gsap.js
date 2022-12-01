4b29a10f818f4ada4e19c15bdcaf928766442d66
Luca { Emanuel } <lucaemanuel7x7@gmail.com>
2022-12-01 22:21:57 +0100

    linked mp3 online & visual polishes

--- gsap.js
+++ gsap.js
@@ -1,13 +1,15 @@
+//import { gsap } from "gsap";
 const menuBtn = document.querySelector(".menu-div");
 const exitBtn = document.querySelector(".exit");
+const nextBtn = document.querySelector("#whatsnext");
 
 let t1 = gsap.timeline({ paused: true });
 
-t1.to(".menu", { opacity: 1, duration: 1, top: 0, ease: Power2.easeInOut });
+t1.to(".menu", { opacity: 1, duration: 1.05, top: 0, ease: Power2.easeInOut });
 t1.to(
 	".nav",
 	{
-		opacity: 1,
+		opacity: 0.49,
 		marginBottom: 0,
 		duration: 1,
 		ease: Power2.easeInOut,
@@ -17,6 +19,9 @@ t1.to(
 );
 
 menuBtn.addEventListener("click", () => {
+	t1.play().timeScale(1);
+});
+nextBtn.addEventListener("click", () => {
 	t1.play().timeScale(1);
 });
 
